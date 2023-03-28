6b4fdffe82e2abb226abf070ae2a0bfad3b72e21
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-28 12:34:46 -0600

    add circuitbreaker implementation

--- src/main/java/com/grid/product/controllers/ProductController.java
+++ src/main/java/com/grid/product/controllers/ProductController.java
@@ -4,8 +4,11 @@ import com.grid.product.assemblers.ProductAssembler;
 import com.grid.product.converters.ProductMapper;
 import com.grid.product.models.dtos.ProductDto;
 import com.grid.product.services.ProductService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,14 +27,25 @@ public class ProductController {
 
     private final ProductAssembler productAssembler;
 
+    @CircuitBreaker(name = "allCB", fallbackMethod = "fallBackGetByUniqueId")
     @GetMapping("/getByUniqueId/{uniqueId}")
-    public EntityModel<ProductDto> getByUniqueId(@PathVariable String uniqueId){
-        return productAssembler.toModel(productMapper.modelToDto(productService.getByUniqueId(uniqueId)));
+    public ResponseEntity<EntityModel<ProductDto>> getByUniqueId(@PathVariable String uniqueId){
+        return new ResponseEntity<>(productAssembler.toModel(productMapper.modelToDto(productService.getByUniqueId(uniqueId))),HttpStatus.OK);
     }
 
+    @CircuitBreaker(name = "allCB", fallbackMethod = "fallBackGetBySku")
     @GetMapping("/getBySku/{sku}")
-    public List<EntityModel<ProductDto>> getBySku(@PathVariable String sku){
-        return productService.getBySku(sku).stream().map(productMapper::modelToDto).map(productAssembler::toModel).toList();
+    public List<ResponseEntity<EntityModel<ProductDto>>> getBySku(@PathVariable String sku){
+        return productService.getBySku(sku).stream().map(productMapper::modelToDto).map(productAssembler::toModel).map(ResponseEntity::ok).toList();
+    }
+
+    private ResponseEntity<EntityModel<ProductDto>> fallBackGetByUniqueId(@PathVariable String uniqueId,RuntimeException e){
+        return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+
+    private List<ResponseEntity<EntityModel<ProductDto>>> fallBackGetBySku(@PathVariable String sku,RuntimeException e){
+        return List.of(new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE));
     }
 
 }
