389682503c49b11f5e378c31ea8d34a8f66c241b
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-28 12:34:27 -0600

    add builder

--- src/main/java/com/grid/product/models/dtos/ProductDto.java
+++ src/main/java/com/grid/product/models/dtos/ProductDto.java
@@ -1,10 +1,12 @@
 package com.grid.product.models.dtos;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class ProductDto {
     private String uniqId;
 
