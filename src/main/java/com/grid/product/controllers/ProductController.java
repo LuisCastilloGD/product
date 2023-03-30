5e6606f30a5a0984a0f4591ec5d96355a347de44
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-30 11:09:31 -0600

    change to now include CircuitBreaker implementation with fallback methods

--- src/main/java/com/grid/product/controllers/ProductController.java
+++ src/main/java/com/grid/product/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.grid.product.controllers;
 
 import com.grid.product.assemblers.ProductAssembler;
 import com.grid.product.converters.ProductMapper;
+import com.grid.product.exceptions.ProductNotAvailableException;
+import com.grid.product.exceptions.ProductNotFoundedException;
 import com.grid.product.models.dtos.ProductDto;
 import com.grid.product.services.ProductService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
@@ -27,25 +29,33 @@ public class ProductController {
 
     private final ProductAssembler productAssembler;
 
-    @CircuitBreaker(name = "allCB", fallbackMethod = "fallBackGetByUniqueId")
+    @CircuitBreaker(name = "productCB", fallbackMethod = "fallBackGetByUniqueId" )
     @GetMapping("/getByUniqueId/{uniqueId}")
     public ResponseEntity<EntityModel<ProductDto>> getByUniqueId(@PathVariable String uniqueId){
         return new ResponseEntity<>(productAssembler.toModel(productMapper.modelToDto(productService.getByUniqueId(uniqueId))),HttpStatus.OK);
     }
 
-    @CircuitBreaker(name = "allCB", fallbackMethod = "fallBackGetBySku")
+    @CircuitBreaker(name = "productCB", fallbackMethod = "fallBackGetBySku")
     @GetMapping("/getBySku/{sku}")
     public List<ResponseEntity<EntityModel<ProductDto>>> getBySku(@PathVariable String sku){
         return productService.getBySku(sku).stream().map(productMapper::modelToDto).map(productAssembler::toModel).map(ResponseEntity::ok).toList();
     }
 
     private ResponseEntity<EntityModel<ProductDto>> fallBackGetByUniqueId(@PathVariable String uniqueId,RuntimeException e){
-        return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+
+        if(e.getClass().equals(ProductNotFoundedException.class)){
+            throw new ProductNotFoundedException();
+        }if(e.getClass().equals(ProductNotAvailableException.class)) {
+            throw new ProductNotAvailableException();
+        }else{
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+        }
     }
 
 
     private List<ResponseEntity<EntityModel<ProductDto>>> fallBackGetBySku(@PathVariable String sku,RuntimeException e){
         return List.of(new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE));
     }
+
 
 }
