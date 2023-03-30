1d411552f34422c6976ff612c5e8e771e0d41084
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-30 11:05:19 -0600

    add builder

--- src/main/java/com/grid/product/models/Article.java
+++ src/main/java/com/grid/product/models/Article.java
@@ -5,12 +5,14 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 
 @Data
+@Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 public class Article {
--- src/main/java/com/grid/product/models/Item.java
+++ src/main/java/com/grid/product/models/Item.java
@@ -4,11 +4,13 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Item {
