decb99d84abf4ca2207f17dfadd47e7814817cf5
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-30 11:06:15 -0600

    refactored getBySku implementation

--- src/main/java/com/grid/product/services/implementations/ProductServiceImpl.java
+++ src/main/java/com/grid/product/services/implementations/ProductServiceImpl.java
@@ -47,28 +47,24 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<Product> getBySku(final String sku) {
         List<Article> articles = catalogFeignClient.getProductBySku(sku);
-         return articles.stream()
-                        .map(article -> {
-                            Item item = inventoryFeignClient.getItemByUniqueId(article.getUniqId());
-                            assert item != null;
-                            if(item.getAvailability() <= 0){
-                                throw new ProductNotAvailableException();
-                            }else {
-                                return Product.builder()
-                                              .uniqId(article.getUniqId())
-                                              .sku(article.getSku())
-                                              .availability(item.getAvailability())
-                                              .name_title(article.getName_title())
-                                              .description(article.getDescription())
-                                              .list_price(article.getList_price())
-                                              .sale_price(article.getSale_price())
-                                              .category(article.getCategory())
-                                              .category_tree(article.getCategory_tree())
-                                              .product_url(article.getProduct_url())
-                                              .average_product_rating(article.getAverage_product_rating())
-                                              .brand(article.getBrand())
-                                              .build();
-                            }
-        }).toList();
+        return articles.stream()
+                       .map(article -> {
+                           Item item = inventoryFeignClient.getItemByUniqueId(article.getUniqId());
+                           assert item != null;
+                           return Product.builder()
+                                         .uniqId(article.getUniqId())
+                                         .sku(article.getSku())
+                                         .availability(item.getAvailability())
+                                         .name_title(article.getName_title())
+                                         .description(article.getDescription())
+                                         .list_price(article.getList_price())
+                                         .sale_price(article.getSale_price())
+                                         .category(article.getCategory())
+                                         .category_tree(article.getCategory_tree())
+                                         .product_url(article.getProduct_url())
+                                         .average_product_rating(article.getAverage_product_rating())
+                                         .brand(article.getBrand())
+                                         .build();
+                       }).filter(product -> product.getAvailability()>0L).toList();
     }
 }
